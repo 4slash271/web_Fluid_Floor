467ba0be4cb74c381d570742b2efa7be1cd41451
“4slash271” <4slash271@gmail.com>
2022-01-30 02:43:35 +0900

    edited modal

--- js/index.js
+++ js/index.js
@@ -2,16 +2,24 @@ let deactivated = document.querySelectorAll(".deactivated");
 let btn = document.querySelector(".modal__btn");
  let modal = document.querySelector(".modal");
  let modalBox = document.querySelector(".modal__inner");
-function openModal(e){
+ let modalDesc = document.querySelector(".modal__desc");
+function openModal1(e){
 
  modal.style.display="flex";
+ modalDesc.innerHTML = "2022년 3월 1일 이후에 기록됩니다."
+}
+function openModal2(e){
+
+ modal.style.display="flex";
+ modalDesc.innerHTML = "2022년 3월 6일 이후에 기록됩니다."
+
 }
 
 function closeModal(e){
     modal.style.display="none";
 }
-deactivated[0].addEventListener('click',openModal);
-deactivated[1].addEventListener('click',openModal);
+deactivated[0].addEventListener('click',openModal1);
+deactivated[1].addEventListener('click',openModal2);
 modal.addEventListener('click',closeModal)
 btn.addEventListener('click',closeModal)
 
