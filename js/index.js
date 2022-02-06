9c9b57cc336c0e0a44fb89485ff2be5f7d43d348
“4slash271” <4slash271@gmail.com>
2022-02-06 22:53:47 +0900

    start video click with jQuery

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 let deactivated = document.querySelectorAll(".deactivated");
+
+let mainText = document.querySelectorAll(".text__desc");
+let $video = $(".video__screen");
+console.log($video);
+
 let btn = document.querySelector(".modal__btn");
- let modal = document.querySelector(".modal");
- let modalBox = document.querySelector(".modal__inner");
- let modalDesc = document.querySelector(".modal__desc");
+let modal = document.querySelector(".modal");
+let modalBox = document.querySelector(".modal__inner");
+let modalDesc = document.querySelector(".modal__desc");
+
+
+
+// var index = arr3.findIndex(i => i.name == "강호동"); 
+
+
+
 function openModal1(e){
 
  modal.style.display="flex";
@@ -20,6 +32,13 @@ function closeModal(e){
 }
 deactivated[0].addEventListener('click',openModal1);
 deactivated[1].addEventListener('click',openModal2);
+
+$video.click(function(){
+    console.log($(this).find('.text__desc'));
+    $('.text__desc').slideToggle(".active")
+});
+
+
 modal.addEventListener('click',closeModal)
 btn.addEventListener('click',closeModal)
 
