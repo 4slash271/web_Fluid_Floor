f609dc94a7897c3cde3bcb8c465a4a2886a728a2
4slash271 <4slash271@gmail.com>
2022-02-24 00:44:59 +0900

    finished to add loading page in info

--- public/js/info.js
+++ public/js/info.js
@@ -5,6 +5,10 @@
 
 let mobileBtn = document.querySelector(".article__mobile");
 let mobileNav = document.querySelector(".article__links")
+let body =document.body;  
+let loader = document.querySelector(".loader"); 
+let loaderImg = document.querySelector(".loader img"); 
+
 
 /******************************* 사용자 함수 *******************************/
 /******************************* 이벤트 등록 ******************************/
@@ -13,6 +17,24 @@ mobileBtn.addEventListener('mouseover', openMobile);
 mobileBtn.addEventListener('mouseleave', closeMobile);
 
 /******************************* 이벤트 콜백 *****************************/
+
+window.onbeforeunload = function () { 
+    setTimeout(showLoader, 500);
+    function showLoader(){
+        loader.style.visibility = "visible";
+        body.style.overflowY = "hidden";
+    }
+}  //현재 페이지에서 다른 페이지로 넘어갈 때 표시해주는 기능
+window.onload= function(){
+    setTimeout(hideLoader, 1500);
+    function hideLoader(){
+        console.log("hi");
+        loader.style.visibility = "hidden";
+        body.style.overflowY = "unset";
+       
+    }   
+    
+}
 
 function openMobile(e){
       mobileNav.style.display = "flex";
