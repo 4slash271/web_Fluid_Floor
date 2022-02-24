58e6628cdd8f47d0b9ffc78359d9db7fd691e4d1
4slash271 <4slash271@gmail.com>
2022-02-24 14:52:05 +0900

    deleted loader in info

--- public/js/info.js
+++ public/js/info.js
@@ -18,22 +18,7 @@ mobileBtn.addEventListener('mouseleave', closeMobile);
 
 /******************************* 이벤트 콜백 *****************************/
 
-window.onbeforeunload = function () { 
-    setTimeout(showLoader, 500);
-    function showLoader(){
-        loader.style.visibility = "visible";
-        body.style.overflowY = "hidden";
-    }
-}  //현재 페이지에서 다른 페이지로 넘어갈 때 표시해주는 기능
-window.onload= function(){
-    setTimeout(hideLoader, 1500);
-    function hideLoader(){
-        loader.style.visibility = "hidden";
-        body.style.overflowY = "unset";
-       
-    }   
-    
-}
+
 
 function openMobile(e){
       mobileNav.style.display = "flex";
